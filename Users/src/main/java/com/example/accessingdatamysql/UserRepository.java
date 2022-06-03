eedd6fa507e71814c02fbf06f1198bfda65503d9
jsromero98 <84394100+jsromero98@users.noreply.github.com>
2022-06-03 17:44:07 -0500

    removing redundant files

--- Users/src/main/java/com/example/accessingdatamysql/UserRepository.java
+++ Users/src/main/java/com/example/accessingdatamysql/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.accessingdatamysql;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.accessingdatamysql.User;
@@ -7,7 +8,7 @@ import com.example.accessingdatamysql.User;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByEmail(String email);
 
